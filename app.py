0bbfaeed5803202141d2c2ce8df1a23032fc38b5
Syed Muhammad Zaki <szaki1871993@gmail.com>
2025-07-08 21:03:15 +0500

    new updated 1

--- app.py
+++ app.py
@@ -522,7 +522,6 @@ else:
             #     caption = st.text_area("Caption", height=100)
 
         # elif caption_mode == "AI-Generated":
-
             if image and final_image_path:
 
                 # Only generate caption if not already generated
